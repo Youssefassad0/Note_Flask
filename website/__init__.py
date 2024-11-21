838e28cbd3070ef5f38cda8c0011bcfd70b3a29b
Youssefassad0 <amine150assad@gmail.com>
2024-11-21 14:45:10 +0100

    wokring on sign up

--- website/__init__.py
+++ website/__init__.py
@@ -11,10 +11,14 @@ def create_app():
     app.config['SECRET_KEY']=os.urandom(24)  
     app.config['SQLALCHEMY_DATABASE_URI']='mssql+pyodbc://ECM:.\Ec@Extr@net@192.168.2.230/NOTEBOOK?driver=SQL Server Native Client 10.0'
     db.init_app(app) 
+    
     from .views import views
     from .auth import auth
+    
     app.register_blueprint(views,url_prefix='/')
     app.register_blueprint(auth,url_prefix='/')
+    
+    from .models import User,Note
     
 
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, flash
-
+from .models import User
+from werkzeug.security import generate_password_hash,check_password_hash
 auth = Blueprint("auth", __name__)
 
 
@@ -27,6 +28,7 @@ def signup():
         elif role == "":
             flash("Role Must be Selected .", category="error")
         else:
+            new_user=User(email=email,name=name,password=generate_password_hash(password),methode='sha256')
             flash("You created the user correctly !", category="success")
     return render_template("signup.html")
 
