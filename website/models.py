fe9704eec85536578cb2997ce45d6d6e0497a0dc
Youssefassad0 <amine150assad@gmail.com>
2024-11-21 14:29:11 +0100

    corretc the connection

--- website/models.py
+++ website/models.py
@@ -1,1 +1,14 @@
 from . import db
+from flask_login import UserMixin
+
+class User_rentway(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, )
+    name = db.Column(db.String(100), unique=True, )
+    password = db.Column(db.String(100), nullable=False)
+class Note (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date=db.Column(db.DateTime(timezone=True),default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
